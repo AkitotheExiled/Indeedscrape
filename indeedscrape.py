a4d2094086fddc3a76387ca8d76f88b123771080
AkitotheExiled <myguestmike@gmail.com>
2019-04-16 18:38:54 -0700

    Implemented PEP8 naming conventions,whitespace,variable fixes to code.
    Fixed run logic by adding
    "If not self.job:
    break
    which will break the loop if self.job has an empty string(or no response)."

--- indeedscrape.py
+++ indeedscrape.py
@@ -1,26 +1,30 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support.ui import Select
 import time
 import pandas as pd
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+
 
 class Indeed:
+
+
     def __init__(self):
+
+        self.driver = webdriver.Chrome()
         self.job = None
-        self.driver = webdriver.Chrome(executable_path='C:\\chromedriver\\chromedriver.exe')
-        self.url = 'https://www.indeed.com'
         print('Type the location you want to search for( e.g. San Mateo, CA)')
         self.location = input()
         self.scraped_data = []
-        self.done = False
+        self.url = 'https://www.indeed.com'
         self.wait = WebDriverWait(self.driver, 10)
 
-    def ElementDetec(self,element1,element2):
+    def element_detect(self, element1, element2):
+        ''' This method is to be used when you know the name of an input and its ID changes on a different page but that input function remains the same.'''
         try:
             self.driver.find_element_by_id(element1)
         except NoSuchElementException:
@@ -32,73 +36,78 @@ class Indeed:
         return element1
 
     def is_element_here(self, path, element):
+
         pass
 
-    def BrowerOp(self):
+    def browse_web(self):
+
         self.driver.get(self.url)
         time.sleep(10)
 
-    def SearchJob(self):
+    def search_job(self):
 
-        searchField = self.driver.find_element_by_id(self.ElementDetec('text-input-what','what'))
-        searchWhere = self.driver.find_element_by_id(self.ElementDetec('text-input-where','where'))
+        search_field = self.driver.find_element_by_id(
+            self.element_detect('text-input-what', 'what')
+        )
+        search_where = self.driver.find_element_by_id(
+            self.element_detect('text-input-where', 'where')
+        )
 
-        searchField.send_keys(Keys.CONTROL + "a")
-        searchField.send_keys(Keys.DELETE)
-        searchWhere.send_keys(Keys.CONTROL + "a")
-        searchWhere.send_keys(Keys.DELETE)
+        search_field.send_keys(Keys.CONTROL + "a")
+        search_field.send_keys(Keys.DELETE)
+        search_where.send_keys(Keys.CONTROL + "a")
+        search_where.send_keys(Keys.DELETE)
+        search_where.send_keys(self.location)
+        search_field.send_keys(self.job)
+        search_field.submit()
 
-        searchWhere.send_keys(self.location)
-        searchField.send_keys(self.job)
+    def scrape_page(self):
 
-        searchField.submit()
-
-    def ScrapePage(self):
         try:
-            #containers = self.wait.until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class,'jobsearch-SerpJobCard unifiedRow row result clickcard')]")))
-            #containers = self.driver.find_elements_by_xpath("//div[contains(@class,'jobsearch-SerpJobCard unifiedRow row result clickcard')]")
-            containers = self.driver.find_elements_by_xpath("//div[contains(@class,'jobsearch-SerpJobCard unifiedRow row result clickcard')]")
+            # containers = self.wait.until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class,'jobsearch-SerpJobCard unifiedRow row result clickcard')]")))
+            # containers = self.driver.find_elements_by_xpath("//div[contains(@class,'jobsearch-SerpJobCard unifiedRow row result clickcard')]")
+            containers = self.driver.find_elements_by_xpath(
+                "//div[contains(@class,'jobsearch-SerpJobCard unifiedRow row result clickcard')]"
+            )
         except NoSuchElementException:
             print('A certain element cannot be found.')
-        for title in containers:
-            job_name = title.find_element_by_class_name('title').text
-            job_link = title.find_element_by_class_name('title').find_element_by_tag_name('a').get_attribute('href')
-            company = title.find_element_by_class_name('company').text
-            location = title.find_element_by_class_name('location').text
-            self.scraped_data.append({"Job": job_name, "Job Link": job_link, "Company": company, "Location": location})
+        else:
+            for title in containers:
+                job_name = title.find_element_by_class_name('title').text
+                job_link = title.find_element_by_class_name('title').find_element_by_tag_name('a').get_attribute('href')
+                company = title.find_element_by_class_name('company').text
+                location = title.find_element_by_class_name('location').text
+                self.scraped_data.append(
+                    {"Job": job_name, "Job Link": job_link, "Company": company,
+                     "Location": location}
+                )
 
-
-
-
-    def SaveJobs(self):
+    def save_jobs(self):
 
         df = pd.DataFrame(self.scraped_data)
-
         print("data saved...")
         df.to_csv('indeedJobs.csv')
         self.driver.quit()
 
-    def CloseD(self):
+    def close_prog(self):
 
         return self.driver.quit()
 
-    def RunLogic(self):
+    def run_logic(self):
 
-        self.BrowerOp()
-
-        while not self.done:
-            if self.job == "":
-                self.done = True
+        self.browse_web()
+        while True:
+            print(
+                "Enter the job you want to search( or press enter to stop searching)."
+            )
+            self.job = input()
+            if not self.job:
                 break
-            else:
-                print("Enter the job you want to search( or press enter to stop searching).")
-                self.job = input()
-                self.SearchJob()
-                self.ScrapePage()
-
-        self.SaveJobs()
+            self.search_job()
+            self.scrape_page()
+        self.save_jobs()
 
 
 if __name__ == "__main__":
     Srch = Indeed()
-    Srch.RunLogic()
+    Srch.run_logic()
